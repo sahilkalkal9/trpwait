fa997cfbdb12cb4a540a298e3b0dc258f641430f
Sahil Kalkal <sahilkalkal108@gmail.com>
2024-08-17 22:05:48 +0530

    waiting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import fb from "./fb.png"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={fb} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>
+            Site temporarily down for scheduled improvements. <br /> Exciting updates on the way
+          </code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
